de32099cfa0c4d936c71b5039aa2e4441b55af55
Konstantin Käfer <mail@kkaefer.com>
2018-09-24 13:46:17 +0200

    update

--- index.js
+++ index.js
@@ -1,38 +1,48 @@
 const jwt = require('jsonwebtoken');
 const github = require('@octokit/rest')();
 
+const CODE_CHECK_APP_ID = 17977;
+const CODE_CHECK_APP_INSTALLATION_ID = 348102;
+
 process.on('unhandledRejection', error => {
     console.log(error);
     process.exit(1)
 });
 
 async function run() {
-    const key = Buffer.from(process.env['APP_PRIVATE_KEY'], 'base64').toString('binary');
+    const key = Buffer.from(process.env['CODE_CHECK_PRIVATE_KEY'], 'base64').toString('binary');
     const payload = {
         exp: Math.floor(Date.now() / 1000) + 60,
         iat: Math.floor(Date.now() / 1000),
-        iss: 14028
+        iss: CODE_CHECK_APP_ID
     };
 
-    const token = jwt.sign(payload, key, {algorithm: 'RS256'});
+    const token = jwt.sign(payload, key, { algorithm: 'RS256' });
     github.authenticate({type: 'app', token});
 
-    const res = await github.apps.createInstallationToken({installation_id: '220321'});
+    const res = await github.apps.createInstallationToken({ installation_id: CODE_CHECK_APP_INSTALLATION_ID });
     github.authenticate({type: 'token', token: res.data.token});
 
     await github.checks.create({
-        owner: 'jfirebaugh',
+        owner: 'kkaefer',
         repo: 'github-app',
         name: 'test-check',
         head_branch: process.env['CIRCLE_BRANCH'],
         head_sha: process.env['CIRCLE_SHA1'],
-        status: 'completed',
-        conclusion: 'success',
+        conclusion: 'neutral',
         completed_at: new Date().toISOString(),
         output: {
             title: 'This is the output title',
-            summary: 'Congratulations @jfirebaugh, your check completed successfully',
-            text: 'This is more text. It also supports markdown.\n* A\n* B\n* C'
+            summary: 'Congratulations @kkaefer, your check completed successfully',
+            annotations: [{
+                path: 'index.js',
+                start_line: 4,
+                end_line: 5,
+                annotation_level: 'notice',
+                message: 'missing whitespace',
+                title: 'asdf',
+                raw_details: 'even more details',
+            }]
         }
     });
 }
